e0d928dfea47278c1198310da5f6d8c70e8d0fa5
Tyastrab <38965052+Tyastrab@users.noreply.github.com>
2020-01-06 10:29:56 -0500

    Update C_program.c

--- Project_Files/C_program.c
+++ Project_Files/C_program.c
@@ -6,32 +6,28 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(){
+
+int main(int argc, char *argv[]){
   char word[20];
   printf("Enter the word you're searchign for: ");
-  scanf("%s", word);
-	pid_t pid;
-	int fd[2]; //fileDecriptor for read(0) & write(1)
-	char* cat_arguments[] = {"cat", "roshei.txt", NULL};
-	char* grep_arguments[] = {"grep", word, NULL};
-	pipe(fd); //add vals of FD to main's File Table
-	pid = fork();
-	if(pid==0){ //sucessfully created a child process
-	        close(1); //close stdout
-		dup(fd[1]); 
-		close(fd[0]); //otherwise program will keep trying to read 
-		execvp("cat", cat_arguments);
-	}
-	pid = fork();
-	if(pid==0){ 
-		close(0);
-		close(fd[1]);
-		dup(fd[0]);
-		execvp("grep", grep_arguments);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	wait(NULL);
-	wait(NULL);
-	return 0;
+  scanf("%s", word); //word = user's search term
+  
+  int fd[2]; //File Descriptor, where fd[0]=read & fd[1]=write
+  pipe(fd); //set up a pipe between the read and write
+  int pid2, pid1 = fork(); //create 2 forks for 2 child processes
+  if (pid1 == 0){ //if child is successfully forked:
+    close(fd[0]); //close the end of the FD not in use (read in this case)
+    dup2(fd[1],1); //tie write end of process to write end of pipe
+    execlp("cat","cat","sample.txt",NULL); //perform the cat
+  }
+  else{
+    close(fd[1]); //close the end of the FD not in use (write in this case)
+    dup2(fd[0],0); //tie read end of process to read end of pipe 
+    execlp("grep","grep",word,NULL); //perform the grep
+  }
+  close(fd[0]); //close the read/write process
+  close(fd[1]);
+  wait(NULL);
+  wait(NULL);
+  return 0;
 }
